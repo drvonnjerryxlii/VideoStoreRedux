3e692ff6cb94016fe81d89cc7062a525eb62cf02
drvonnjerryxlii <drvonnjerryxlii@gmail.com>
2015-11-09 13:06:54 -0800

    added properties to customer, movie, and rental models

---
+++ VideoStoreRedux/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoStoreRedux.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public double AccountCredit { get; set; }
+        public double AccountBalance { get; set; }
+    }
+}
---
+++ VideoStoreRedux/Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoStoreRedux.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int CustomerId { get; set; }
+        public bool Returned { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+    }
+}
