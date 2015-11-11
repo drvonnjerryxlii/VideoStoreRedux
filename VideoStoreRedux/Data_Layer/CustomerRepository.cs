46a42192c060624f2613b44b1a4a4bbd332a950c
drvonnjerryxlii <drvonnjerryxlii@gmail.com>
2015-11-10 16:20:41 -0800

    merging in code from atom

--- VideoStoreRedux/Data_Layer/CustomerRepository.cs
+++ VideoStoreRedux/Data_Layer/CustomerRepository.cs
@@ -14,10 +14,15 @@ namespace VideoStoreRedux.Data_Layer
         // database connection
         private IDbConnection db = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFileName=C:\Users\AdaRockstar\Documents\visual studio 2015\Projects\VideoStoreRedux\VideoStoreRedux\App_Data\VideoStoreDB-Development.mdf;Integrated Security=True");
 
+        // dapper params reference:
+        // http://stackoverflow.com/questions/9481678/how-to-create-arguments-for-a-dapper-query-dynamically
+
         // Create
         #region CustomerRepository.Create
         public Customer Create(Customer customer)
         {
+            //string[] allowedParams = { "name", "registered_at", "address", "city", "state", "zip", "phone", "account_credit", "account_balance" };
+            //return this.db.Query<Customer>("insert into customer ", dbArgs);
             throw new NotImplementedException();
         }
         #endregion
@@ -30,6 +35,8 @@ namespace VideoStoreRedux.Data_Layer
         }
         #endregion
 
+        //public static IEnumerable<T> Query<T>(this IDbConnection cnn, string sql, object param = null, SqlTransaction transaction = null, bool buffered = true)
+        //connection.Query<Dog>("select Age = @Age, Id = @Id", new { Age = (int?)null, Id = guid });
         #region CustomerRepository.Find
         public Customer Find(int id)
         {
@@ -58,6 +65,8 @@ namespace VideoStoreRedux.Data_Layer
         #region CustomerRepository.Update
         public Customer Update(Customer customer)
         {
+            //var dbArgs = new DynamicParameters();
+            //foreach (var pair in args) dbArgs.Add(pair.Key, pair.Value);
             throw new NotImplementedException();
         }
         #endregion
